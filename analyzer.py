45b44e82223c58997b6d459c87a88c29f27df644
zoro <s0585804335@gmail.com>
2026-02-11 16:57:20 +0200

    יצרנו בדיקה האם פורט רגיש

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,6 @@
-from checks import internal_address_check
+from checks import check_internal_address,check_sensitive_port
 
-def external_IP_check(data_list):
-    return  [line[1] for line in data_list if internal_address_check(line)]
+def analyzer_internal_address(data_list):
+    return  [line[1] for line in data_list if check_internal_address(line)]
+def analyzer_sensitive_port(data_list):
+    return  [line[3] for line in data_list if check_sensitive_port(line)]
--- checks.py
+++ checks.py
@@ -1,5 +1,10 @@
-from config import EXTERNAL_IP
+from config import EXTERNAL_IP, SENSITIVE_PORT
 
-def internal_address_check(line):
+def check_internal_address(line):
     is_external = any(line[1].startswith(ip) for ip in EXTERNAL_IP)
     return not is_external
+
+def check_sensitive_port(line):
+    if line[3] in SENSITIVE_PORT:
+        return True
+    return False
