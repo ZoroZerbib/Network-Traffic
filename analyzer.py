10fd88f320f2b14509f9108d12de62393a240ba4
zoro <s0585804335@gmail.com>
2026-02-11 17:05:22 +0200

    יצרנו בדיקת גודל חבילה

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,13 @@
-from checks import check_internal_address,check_sensitive_port
+from checks import check_internal_address, check_sensitive_port, check_package_size
+
 
 def analyzer_internal_address(data_list):
-    return  [line[1] for line in data_list if check_internal_address(line)]
+    return [line[1] for line in data_list if check_internal_address(line)]
+
+
 def analyzer_sensitive_port(data_list):
-    return  [line[3] for line in data_list if check_sensitive_port(line)]
+    return [line for line in data_list if check_sensitive_port(line)]
+
+
+def analyzer_package_size(data_list):
+    return [line for line in data_list if check_package_size(line)]
