74d9049c7ae7937f633d32ee57aaba15c785ce6d
zoro <s0585804335@gmail.com>
2026-02-12 17:26:03 +0200

    זיהוי חשדות לכל אייפי

--- analyzer.py
+++ analyzer.py
@@ -14,7 +14,7 @@ def analyzer_package_size(data_list):
 
 
 def analyzer_time(data_list):
-    return [line for line in data_list if check_package_size(line)]
+    return [line for line in data_list if check_time(line)]
 
 
 def labeling_package_size(data_list):
@@ -28,3 +28,23 @@ def request_count_IP1(data_list):
 
 def port_for_protocol_labeling(data_list):
     return {line[3]: line[4] for line in data_list}
+
+def identifying_suspicions_for_each_IP(data_list):
+    suspicions_dict={}
+    def add_dic(ip, condition):
+        if ip not in suspicions_dict:
+            suspicions_dict[ip] = []
+        if condition not in suspicions_dict[ip]:
+            suspicions_dict[ip].append(condition)
+    for line in data_list:
+        ip_new= line[1]
+        if check_internal_address(line):
+            condition_new="IP_EXTERNAL"
+            add_dic(ip_new,condition_new)
+        if check_sensitive_port(line):
+            add_dic(ip_new,"PORT_SENSITIVE")
+        if check_package_size(line):
+            add_dic(ip_new,"PACKET_LARGE")
+        if check_time(line):
+            add_dic(ip_new,"ACTIVITY_NIGHT")
+    return suspicions_dict
