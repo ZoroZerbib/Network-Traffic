d1e15c9f7c4b1b8507cc8f7333cc7f55701f5796
zoro <s0585804335@gmail.com>
2026-02-12 16:02:19 +0200

    יצרנו פונקציה למיפוי פורט וכנראה גם למיספור אייפי

--- analyzer.py
+++ analyzer.py
@@ -19,3 +19,12 @@ def analyzer_time(data_list):
 
 def labeling_package_size(data_list):
     return [line.append("LARGE") if check_package_size(line) else line.append("NORMAL") for line in data_list]
+
+
+def request_count_IP1(data_list):
+    address_IP = [line[1] for line in data_list]
+    return {ip: address_IP.count(ip) for ip in set(address_IP)}
+
+
+def port_for_protocol_labeling(data_list):
+    return {line[3]: line[4] for line in data_list}
